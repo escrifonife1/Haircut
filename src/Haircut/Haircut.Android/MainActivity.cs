e8d817204064d294eb60a0e08bc652c0b6ea0ad2
tiago <tiagopascal@gmail.com>
2016-11-17 20:42:24 -0200

    Created android application

---
+++ src/Haircut/Haircut.Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Haircut.Android
+{
+    [Activity(Label = "Haircut.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
