ad7b282304dc907dad0c246d119442ab7064c98f
tiago <tiagopascal@gmail.com>
2016-11-19 00:15:12 -0200

    Added Xamarin Forms' project

--- src/Haircut/Haircut.Android/MainActivity.cs
+++ src/Haircut/Haircut.Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace Haircut.Android
 {
-    [Activity(Label = "Haircut.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "Haircut", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
@@ -12,7 +12,15 @@ namespace Haircut.Android
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+             SetContentView (Resource.Layout.Main);
+            var button = FindViewById<Button>(Resource.Id.button_test);
+
+            button.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            StartActivity(typeof(LoginActivity));
         }
     }
 }
--- src/Haircut/Haircut.Android/Resources/Resource.Designer.cs
+++ src/Haircut/Haircut.Android/Resources/Resource.Designer.cs
@@ -57,11 +57,51 @@ namespace Haircut.Android
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050006
+			public const int button1 = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int button_test = 2131034119;
+			
+			// aapt resource value: 0x7f050002
+			public const int editText1 = 2131034114;
+			
+			// aapt resource value: 0x7f050005
+			public const int editText2 = 2131034117;
+			
+			// aapt resource value: 0x7f050000
+			public const int linearLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int linearLayout3 = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int textView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int textView2 = 2131034116;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int Login = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
