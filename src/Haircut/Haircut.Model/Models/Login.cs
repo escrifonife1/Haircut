0dc1e089d1d57a3acfdfa508c32e0ed442facb82
tiago <tiagopascal@gmail.com>
2016-11-19 22:52:16 -0200

    Adding project to connect to database

--- src/Haircut/Haircut.Model/Models/Login.cs
+++ src/Haircut/Haircut.Model/Models/Login.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Haircut.Core.Models
+namespace Haircut.Model.Models
 {
     public class Login
     {
--- src/Haircut/HaircutWebApi/Controllers/LoginController.cs
+++ src/Haircut/HaircutWebApi/Controllers/LoginController.cs
@@ -1,4 +1,4 @@
-﻿using Haircut.Core.Models;
+﻿using Haircut.Model.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
