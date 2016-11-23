0c77966fe97b782738a0148c87454c652c1c6935
tiago <tiagopascal@gmail.com>
2016-11-23 19:54:40 -0200

    Generates schedule

--- src/Haircut/HaircutWebApi/Controllers/ScheduleController.cs
+++ src/Haircut/HaircutWebApi/Controllers/ScheduleController.cs
@@ -38,5 +38,39 @@ namespace HaircutWebApi.Controllers
 
             return Ok();
         }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var lastSchedule = _scheduleRepository.GetFromDate(DateTime.Today.AddHours(8)).OrderBy(s => s.Date).LastOrDefault();
+
+            var horarioInicial = lastSchedule?.Date.Date.AddDays(1) ?? DateTime.Today.AddHours(8);
+            Schedule schedule;
+            for (int i = 0; i < 23; i++)
+            {
+                schedule = new Schedule()
+                {
+                    Available = 1,
+                    Date = horarioInicial,
+                    Login = new Login()
+                    {
+                        Id = 7,
+                        Name = "i",
+                        UserName = "i",
+                        Created = DateTime.Now,
+                        Password = "i",
+                        Phone = "3"
+                    }
+                };
+                                
+                horarioInicial = horarioInicial.AddMinutes(30);
+                _scheduleRepository.Add(schedule);
+                _scheduleRepository.Save();
+            }
+
+            
+
+            return Ok();
+        }
     }
 }
